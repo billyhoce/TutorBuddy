94821ffb553c3771391a79bf5f22958820b39c76
billyhoce <billyhoce@gmail.com>
2024-10-18 18:36:20 +0800

    Install AuthJS v5 and Google OAuth

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import SignIn from "../components/sign-in"
+
 export default function Home() {
-  return <h1>Welcome To TutorBuddy &#40;Name to be changed&#41;</h1>
+  return (
+    <div>
+      <h1>Welcome To TutorBuddy &#40;Name to be changed&#41;</h1>
+      <SignIn></SignIn>
+    </div>
+  )
 }
